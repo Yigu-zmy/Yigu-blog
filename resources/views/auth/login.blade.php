7a1962b2cc7196ba1e93dd44a1ddef9073698ade
Yigu-zmy <479774965@qq.com>
2017-05-16 01:06:35 +0800

    简单修改了登录注册界面

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,37 @@
-<!-- resources/views/auth/login.blade.php -->
+@extends('home.base')
 
-<form method="POST" action="/auth/login">
-    {!! csrf_field() !!}
-
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
+@section('content')
+    <div class="container" style="margin-top: 20%">
+        <div class="row clearfix">
+            <div class="col-md-12 column">
+                <form class="form-horizontal" role="form" method="POST" action="/auth/login">
+                    {!! csrf_field() !!}
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Email : </label>
+                        <div class="col-sm-5">
+                            <input type="email" name="email" value="{{ old('email') }}" class="form-control" id="inputEmail3">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Password : </label>
+                        <div class="col-sm-5">
+                            <input type="password" name="password" id="password" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-5">
+                            <div class="checkbox">
+                                <label><input type="checkbox" name="remember">Remember me</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-default">Sign in</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-
-    <div>
-        Password
-        <input type="password" name="password" id="password">
-    </div>
-
-    <div>
-        <input type="checkbox" name="remember"> Remember Me
-    </div>
-
-    <div>
-        <button type="submit">Login</button>
-    </div>
-</form>
+    @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,27 +1,46 @@
-<form method="POST" action="/auth/register">
-    {!! csrf_field() !!}
+@extends('home.base')
 
-    <div>
-        Name
-        <input type="text" name="name" value="{{ old('name') }}">
-    </div>
+@section('content')
+    <div class="container" style="margin-top: 20%">
+        <div class="row clearfix">
+            <div class="col-md-12 column">
+                <form class="form-horizontal" role="form" method="POST" action="/auth/register">
+                    {!! csrf_field() !!}
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
+                    <div class="form-group">
+                        <label  class="col-sm-2 control-label">Name : </label>
+                        <div class="col-sm-5">
+                            <input type="text" name="name" value="{{ old('name') }}" class="form-control">
+                        </div>
+                    </div>
 
-    <div>
-        Password
-        <input type="password" name="password">
-    </div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Email : </label>
+                        <div class="col-sm-5">
+                            <input type="email" name="email" value="{{ old('name') }}" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Password : </label>
+                        <div class="col-sm-5">
+                            <input type="password" name="password" value="{{ old('name') }}" class="form-control">
+                        </div>
+                    </div>
 
-    <div>
-        Confirm Password
-        <input type="password" name="password_confirmation">
-    </div>
+                    <div class="form-group">
+                        <label  class="col-sm-2 control-label">Confirm Password : </label>
+                        <div class="col-sm-5">
+                            <input type="password" name="password_confirmation"  class="form-control">
+                        </div>
+                    </div>
 
-    <div>
-        <button type="submit">Register</button>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-default">Register</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-</form>
+@endsection
