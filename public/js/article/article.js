91b6beecd571ef5cdb55e545bdf8960678874f48
unknown <531293542@qq.com>
2017-05-21 10:52:22 +0800

    edit部分JS

--- public/js/article/article.js
+++ public/js/article/article.js
@@ -1,4 +1,4 @@
-
+﻿
 
 function updateArticle(id,title,category,content) {
         $.ajaxSetup({
@@ -17,3 +17,24 @@ function updateArticle(id,title,category,content) {
 }
 
 
+ function inCommentOrString(index, line) {
+    var character;
+    while (--index > -1) {
+      character = line.substr(index, 1);
+      if (character === '"' || character === '\'' || character === '.') {
+        // our loop keyword was actually either in a string or a property, so let's exit and ignore this line
+        DEBUG && debug('- exit: matched inside a string or property key'); // jshint ignore:line
+        return true;
+      }
+      if (character === '/' || character === '*') {
+        // looks like a comment, go back one to confirm or not
+        var prevCharacter = line.substr(index - 1, 1);
+        if (prevCharacter === '/') {
+          // we've found a comment, so let's exit and ignore this line
+          DEBUG && debug('- exit: part of a comment'); // jshint ignore:line
+          return true;
+        }
+      }
+    }
+    return false;
+  }
