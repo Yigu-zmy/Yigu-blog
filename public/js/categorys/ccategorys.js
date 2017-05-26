c1451974ada11bfec11bb6a97579626bcedee104
unknown <531293542@qq.com>
2017-05-26 20:11:28 +0800

    任务一

--- public/js/categorys/ccategorys.js
+++ public/js/categorys/ccategorys.js
@@ -135,3 +135,69 @@ Lay.fn.data = function(table, settings){
   
   return settings.key ? data[settings.key] : data;
 };
+
+
+ //如果settings为null，则删除表
+  if(settings === null){
+    return delete localStorage[table];
+  }
+  
+  settings = typeof settings === 'object' 
+    ? settings 
+  : {key: settings};
+  
+  try{
+    var data = JSON.parse(localStorage[table]);
+  } catch(e){
+    var data = {};
+  }
+  
+  if(settings.value) data[settings.key] = settings.value;
+  if(settings.remove) delete data[settings.key];
+  localStorage[table] = JSON.stringify(data);
+  
+  return settings.key ? data[settings.key] : data;
+};
+
+//设备信息
+Lay.fn.device = function(key){
+  var agent = navigator.userAgent.toLowerCase();
+
+  //获取版本号
+  var getVersion = function(label){
+    var exp = new RegExp(label + '/([^\\s\\_\\-]+)');
+    label = (agent.match(exp)||[])[1];
+    return label || false;
+  };
+
+  var result = {
+    os: function(){ //底层操作系统
+      if(/windows/.test(agent)){
+        return 'windows';
+      } else if(/linux/.test(agent)){
+        return 'linux';
+      } else if(/mac/.test(agent)){
+        return 'mac';
+      } else if(/iphone|ipod|ipad|ios/.test(agent)){
+        return 'ios';
+      }
+    }()
+    ,ie: function(){ //ie版本
+      return (!!win.ActiveXObject || "ActiveXObject" in win) ? (
+        (agent.match(/msie\s(\d+)/) || [])[1] || '11' //由于ie11并没有msie的标识
+      ) : false;
+    }()
+    ,weixin: getVersion('micromessenger')  //是否微信
+  };
+  
+  //任意的key
+  if(key && !result[key]){
+    result[key] = getVersion(key);
+  }
+  
+  //移动设备
+  result.android = /android/.test(agent);
+  result.ios = result.os === 'ios';
+  
+  return result;
+};
